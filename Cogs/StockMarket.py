72adac073984795e18e12d15bb6db905b366f540
HaTinnng <hyeon2896@gmail.com>
2025-03-17 13:10:07 +0900

    StockMarket.py 업데이트

--- Cogs/StockMarket.py
+++ Cogs/StockMarket.py
@@ -307,7 +307,7 @@ class StockMarket(commands.Cog):
     
     def update_stocks(self):
         """
-        모든 주식의 가격을 19.58% ~ -17.12% 변동폭 내에서 변동합니다.
+        모든 주식의 가격을 18.58% ~ -17.12% 변동폭 내에서 변동합니다.
         가격이 13원 미만이 되면 상장폐지되며, 가격이 0원이 되고 변동이 중단됩니다.
         """
         if not self.is_trading_open():
